2eb57e28cb32872ea9cc51580fd74189135b79da
Ahmad Auf Bin Nasruddin <ahmadauf98@gmail.com>
2021-01-21 06:09:13 +0800

    Add realtime feeding courier rating

---
+++ pages/seller/shipment/shipmentsettings.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-app>
+    <div>
+      <div class="section"><sellerAppHero /></div>
+
+      <div>
+        <v-row>
+          <v-col cols="3">
+            <sellerSidebar />
+          </v-col>
+
+          <v-col cols="9"></v-col>
+        </v-row>
+      </div>
+    </div>
+  </v-app>
+</template>
+
+<script>
+import sellerSidebar from '@/components/sellerSidebar'
+import sellerAppHero from '@/components/sellerHero'
+
+export default {
+  components: {
+    sellerAppHero,
+    sellerSidebar,
+  },
+  data() {
+    return {}
+  },
+}
+</script>
