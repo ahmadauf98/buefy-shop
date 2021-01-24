299d80538c10f928146f8d517c0448d688094781
Ahmad Auf Bin Nasruddin <ahmadauf98@gmail.com>
2021-01-25 04:37:15 +0800

    Improve UI of Search Bar

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,47 +1,52 @@
 <template>
-  <div class="row">
-    <div class="col-9 mt-15 pt-5 mb-10">
+  <div class="mt-15 pt-5 px-1 mb-10 d-flex">
+    <!-- Title & Subtitle -->
+    <div>
       <p class="title text-h4 font-weight-medium">
         <strong class="font-title">Welcome</strong> to our awesome market
       </p>
       <p class="subtitle">Below you will find your latests products</p>
     </div>
-    <div class="ml-15 mt-15 pt-5 mb-10">
-      <v-autocomplete
-        class="ml-2"
-        :items="items"
-        :loading="isLoading"
-        :search-input.sync="search"
-        append-icon="mdi-magnify"
-        item-text="name"
-        label="Search"
-        clearable
-        hide-details
-        hide-selected
-        dense
-        outlined
-      >
-        <template v-slot:no-data>
-          <v-list-item>
-            <v-list-item-title>
-              Search for your
-              <strong>Product</strong>
-            </v-list-item-title>
-          </v-list-item>
-        </template>
-        <template v-slot:item="{ item }">
-          <v-list-item :to="`products/${item.product_id}`">
-            <!-- Product Image -->
-            <v-list-item-avatar>
-              <v-img :src="item.image"></v-img>
-            </v-list-item-avatar>
-            <!-- Product Name -->
-            <v-list-item-content>
-              <v-list-item-title v-text="item.name"></v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-        </template>
-      </v-autocomplete>
+
+    <!-- Search Bar -->
+    <div class="col-4 ml-auto pt-0 pr-0">
+      <v-card flat>
+        <v-autocomplete
+          :items="items"
+          :loading="isLoading"
+          :search-input.sync="search"
+          append-icon="mdi-magnify"
+          item-text="name"
+          label="Search"
+          width="300px"
+          clearable
+          hide-details
+          hide-selected
+          dense
+          outlined
+        >
+          <template v-slot:no-data>
+            <v-list-item>
+              <v-list-item-title>
+                Search for your
+                <strong>Product</strong>
+              </v-list-item-title>
+            </v-list-item>
+          </template>
+          <template v-slot:item="{ item }">
+            <v-list-item :to="`products/${item.product_id}`">
+              <!-- Product Image -->
+              <v-list-item-avatar>
+                <v-img :src="item.image"></v-img>
+              </v-list-item-avatar>
+              <!-- Product Name -->
+              <v-list-item-content>
+                <v-list-item-title v-text="item.name"></v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </template>
+        </v-autocomplete>
+      </v-card>
     </div>
   </div>
 </template>
@@ -80,8 +85,9 @@ export default {
           this.loadingOverlay = false
         }, 1000)
     },
-    //search
-    search(val) {
+
+    // Search Bar
+    search() {
       if (this.items.length > 0) return
 
       this.isLoading = true
