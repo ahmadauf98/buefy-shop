1597c87b3c033d88cbf53f49aea5763efcdd81ea
Ahmad Auf Bin Nasruddin <ahmadauf98@gmail.com>
2021-01-26 19:54:48 +0800

    Adding Seller Page (My Shipment)

---
+++ components/Tab/Tab.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="mb-n2">
+    <v-btn
+      to="/seller/order/myorders"
+      class="mr-1 text-subtitle-1 text-capitalize"
+      color="white"
+      depressed
+      >All</v-btn
+    >
+    <v-btn
+      to="/seller/shipment/myshipment"
+      class="mr-1 text-subtitle-1 text-capitalize"
+      color="white"
+      depressed
+      >To Ship</v-btn
+    >
+    <v-btn
+      to="/seller/order/ordershipping"
+      class="mr-1 text-subtitle-1 text-capitalize"
+      color="white"
+      depressed
+      >Shipping</v-btn
+    >
+    <v-btn
+      to="/seller/order/ordercompleted"
+      class="mr-1 text-subtitle-1 text-capitalize"
+      color="white"
+      depressed
+      >Completed</v-btn
+    >
+    <v-btn
+      to="/seller/order/ordercancellation"
+      class="mr-1 text-subtitle-1 text-capitalize"
+      color="white"
+      depressed
+      >Cancellation</v-btn
+    >
+  </div>
+</template>
